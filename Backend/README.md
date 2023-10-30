86fdb70b70211a4a972f3cba9397354e183f610e
= <gada.a.dominic@gmail.com>
2023-10-29 19:32:33 -0700

    Created a Next.js app as well as added an example Django backend piece.

---
+++ Backend/README.md
@@ -0,0 +1,6 @@
+Welcome to our python backend.
+We expect this to be simple.
+
+There will be api calls and end points for you to browse to interact with the website.
+Thanks!
+
---
+++ Backend/earlymornings/earlymorningblog/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class EarlymorningblogConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'earlymorningblog'
